6b96ea2c80842c31103d7d2ec9e806c19b5190d6
Cody Kimb <codykimbell@gmail.com>
2021-03-15 12:57:52 -0500

    finish user model

--- config/connection.js
+++ config/connection.js
@@ -2,6 +2,8 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
+console.log(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW)
+
 // create connection to our db
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
   host: 'localhost',
