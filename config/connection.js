777b70e1e52af5961ae73a385ee868c448951c7b
Cody Kimb <codykimbell@gmail.com>
2021-03-16 14:15:02 -0500

    post model

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ console.log(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW)
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
   host: 'localhost',
   dialect: 'mysql',
-  port: 330654
+  port: 3306
 });
 
 module.exports = sequelize;
