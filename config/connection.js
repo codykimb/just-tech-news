e1d6ea24b989435fa084ec18a489c76568b029ce
Cody Kimb <codykimbell@gmail.com>
2021-03-16 19:13:12 -0500

    post routes

--- config/connection.js
+++ config/connection.js
@@ -2,8 +2,6 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-console.log(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW)
-
 // create connection to our db
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
   host: 'localhost',
@@ -12,3 +10,4 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
 });
 
 module.exports = sequelize;
+
